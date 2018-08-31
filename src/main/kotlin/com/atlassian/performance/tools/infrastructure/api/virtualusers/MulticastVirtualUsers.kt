68e3453006913efd591e512bc64abf83ca5dd0ec
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2018-08-31 14:33:18 +0200

    JPERF-16: Spread load over VU nodes

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/virtualusers/MulticastVirtualUsers.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/virtualusers/MulticastVirtualUsers.kt
@@ -2,6 +2,7 @@ package com.atlassian.performance.tools.infrastructure.api.virtualusers
 
 import com.atlassian.performance.tools.concurrency.submitWithLogContext
 import com.atlassian.performance.tools.jiraactions.scenario.Scenario
+import com.atlassian.performance.tools.virtualusers.VirtualUserLoad
 import com.atlassian.performance.tools.virtualusers.VirtualUserOptions
 import com.google.common.util.concurrent.ThreadFactoryBuilder
 import java.net.URI
@@ -12,7 +13,9 @@ class MulticastVirtualUsers<out T : VirtualUsers>(
 ) : VirtualUsers {
 
     override fun gatherResults() {
-        multicast("gatherResults") { it.gatherResults() }
+        multicast("gatherResults") { node, _ ->
+            node.gatherResults()
+        }
     }
 
     @Deprecated(message = "Do not use.")
@@ -22,12 +25,18 @@ class MulticastVirtualUsers<out T : VirtualUsers>(
         scenarioClass: Class<out Scenario>?,
         diagnosticsLimit: Int?
     ) {
-        multicast("applyLoad") { it.applyLoad(jira, loadProfile, scenarioClass, diagnosticsLimit) }
+        multicast("applyLoad") { node, _ ->
+            node.applyLoad(jira, loadProfile, scenarioClass, diagnosticsLimit)
+        }
     }
 
+    /**
+     * @param label Labels the [operation].
+     * @param operation Operates on a the given node, indexed from 0.
+     */
     private fun multicast(
         label: String,
-        operation: (VirtualUsers) -> Unit
+        operation: (VirtualUsers, Long) -> Unit
     ) {
         val executor = Executors.newFixedThreadPool(
             nodes.size,
@@ -36,12 +45,12 @@ class MulticastVirtualUsers<out T : VirtualUsers>(
                 .build()
         )
         nodes
-            .map {
-                executor.submitWithLogContext("$label $it") {
+            .mapIndexed { index, node ->
+                executor.submitWithLogContext("$label $node") {
                     try {
-                        operation(it)
+                        operation(node, index.toLong())
                     } catch (e: Exception) {
-                        throw Exception("$label failed on $it", e)
+                        throw Exception("$label failed on $node", e)
                     }
                 }
             }
@@ -50,6 +59,25 @@ class MulticastVirtualUsers<out T : VirtualUsers>(
     }
 
     override fun applyLoad(options: VirtualUserOptions) {
-        TODO("not implemented")
+        val nodeCount = nodes.size
+        val load = options.virtualUserLoad
+        val virtualUsers = load.virtualUsers
+        if (nodeCount > virtualUsers) {
+            throw Exception("$virtualUsers virtual users are not enough to spread into $nodeCount nodes")
+        }
+        val vusPerNode = virtualUsers / nodeCount
+        val rampPerNode = load.ramp.dividedBy(nodeCount.toLong())
+        multicast("apply load") { node, index ->
+            node.applyLoad(
+                options.copy(
+                    virtualUserLoad = VirtualUserLoad(
+                        virtualUsers = vusPerNode,
+                        hold = load.hold + rampPerNode.multipliedBy(index),
+                        ramp = rampPerNode,
+                        flat = load.flat + rampPerNode.multipliedBy(nodeCount - index - 1)
+                    )
+                )
+            )
+        }
     }
 }
---
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/virtualusers/MulticastVirtualUsersTest.kt
@@ -0,0 +1,133 @@
+package com.atlassian.performance.tools.infrastructure.api.virtualusers
+
+import com.atlassian.performance.tools.jiraactions.scenario.Scenario
+import com.atlassian.performance.tools.virtualusers.VirtualUserLoad
+import com.atlassian.performance.tools.virtualusers.VirtualUserOptions
+import net.jcip.annotations.NotThreadSafe
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.assertThat
+import org.junit.Test
+import java.net.URI
+import java.time.Duration.ofSeconds
+
+class MulticastVirtualUsersTest {
+    private val load = VirtualUserLoad(
+        virtualUsers = 12,
+        hold = ofSeconds(10),
+        ramp = ofSeconds(16),
+        flat = ofSeconds(55)
+    )
+    private val options = VirtualUserOptions(
+        virtualUserLoad = load
+    )
+
+    @Test
+    fun shouldSpreadLoadOverFourNodes() {
+        val nodes = (1..4).map { SingleUseVirtualUsers() }
+        val virtualUsers = MulticastVirtualUsers(nodes)
+
+        virtualUsers.applyLoad(options)
+
+        assertThat(
+            nodes.map { it.lastOptions!!.virtualUserLoad },
+            hasItems(
+                VirtualUserLoad(
+                    virtualUsers = 3,
+                    hold = ofSeconds(10),
+                    ramp = ofSeconds(4),
+                    flat = ofSeconds(67)
+                ),
+                VirtualUserLoad(
+                    virtualUsers = 3,
+                    hold = ofSeconds(14),
+                    ramp = ofSeconds(4),
+                    flat = ofSeconds(63)
+                ),
+                VirtualUserLoad(
+                    virtualUsers = 3,
+                    hold = ofSeconds(18),
+                    ramp = ofSeconds(4),
+                    flat = ofSeconds(59)
+                ),
+                VirtualUserLoad(
+                    virtualUsers = 3,
+                    hold = ofSeconds(22),
+                    ramp = ofSeconds(4),
+                    flat = ofSeconds(55)
+                )
+            )
+        )
+    }
+
+    @Test
+    fun shouldSpreadTheSameLoad() {
+        val node = SingleUseVirtualUsers()
+        val virtualUsers = MulticastVirtualUsers(listOf(node))
+
+        virtualUsers.applyLoad(options)
+
+        assertThat(node.lastOptions!!.virtualUserLoad, equalTo(load))
+    }
+
+    @Test
+    fun shouldSpreadWithEqualTotals() {
+        val nodes = (1..10).map { SingleUseVirtualUsers() }
+        val virtualUsers = MulticastVirtualUsers(nodes)
+
+        virtualUsers.applyLoad(options)
+
+        nodes
+            .map { it.lastOptions!!.virtualUserLoad }
+            .forEach { assertThat(it.total, equalTo(load.total)) }
+    }
+
+    @Test
+    fun shouldAvoidEmptyNodes() {
+        val tooManyNodes = 300
+        val nodes = (1..tooManyNodes).map { SingleUseVirtualUsers() }
+        val virtualUsers = MulticastVirtualUsers(nodes)
+
+        val exception: Exception? = try {
+            virtualUsers.applyLoad(options)
+            null
+        } catch (e: Exception) {
+            e
+        }
+
+        assertThat(exception, notNullValue())
+        assertThat(
+            exception!!.message,
+            equalTo("12 virtual users are not enough to spread into $tooManyNodes nodes")
+        )
+    }
+
+}
+
+@NotThreadSafe
+private class SingleUseVirtualUsers : VirtualUsers {
+
+    var lastOptions: VirtualUserOptions? = null
+
+    override fun applyLoad(
+        options: VirtualUserOptions
+    ) {
+        if (lastOptions == null) {
+            lastOptions = options
+        } else {
+            throw Exception("Don't apply load multiple times")
+        }
+    }
+
+    override fun applyLoad(
+        jira: URI,
+        loadProfile: LoadProfile,
+        scenarioClass: Class<out Scenario>?,
+        diagnosticsLimit: Int?
+    ) {
+        throw Exception("unexpected call")
+    }
+
+    override fun gatherResults() {
+        throw Exception("unexpected call")
+    }
+}
