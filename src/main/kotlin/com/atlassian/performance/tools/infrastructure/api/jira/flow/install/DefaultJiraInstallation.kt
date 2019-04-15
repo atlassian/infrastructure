ccd85886a1617b32a8430c251154192c7a9aa433
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-04-15 11:25:58 +0200

    Resolve DB TODO
    It's already possible to hook in DB installation from `InstallableDatabase`.

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/flow/install/DefaultJiraInstallation.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/flow/install/DefaultJiraInstallation.kt
@@ -3,13 +3,13 @@ package com.atlassian.performance.tools.infrastructure.api.jira.flow.install
 import com.atlassian.performance.tools.infrastructure.api.distribution.ProductDistribution
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
 import com.atlassian.performance.tools.infrastructure.api.jira.flow.InstalledJira
-import com.atlassian.performance.tools.infrastructure.api.jira.flow.TcpServer
 import com.atlassian.performance.tools.infrastructure.api.jira.flow.JiraNodeFlow
+import com.atlassian.performance.tools.infrastructure.api.jira.flow.TcpServer
 import com.atlassian.performance.tools.infrastructure.api.jvm.JavaDevelopmentKit
 import com.atlassian.performance.tools.ssh.api.SshConnection
 
 class DefaultJiraInstallation(
-    private val jiraHomeSource: JiraHomeSource, // TODO add database too, but how to handle databaseIp? Perhaps as a wrapper, which could be applied by the "DB installing" code?
+    private val jiraHomeSource: JiraHomeSource,
     private val productDistribution: ProductDistribution,
     private val jdk: JavaDevelopmentKit
 ) : JiraInstallation {
