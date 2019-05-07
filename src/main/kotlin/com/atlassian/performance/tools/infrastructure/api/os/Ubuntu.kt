4c184035bfba99860abc8026f72618f31fcf0778
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-05-07 15:57:03 +0200

    JPERF-478: Parallelize Ubuntu installs across SSH hosts

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/os/Ubuntu.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/os/Ubuntu.kt
@@ -13,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap
 class Ubuntu {
 
     private companion object {
-        private val LOCKS = ConcurrentHashMap<SshConnection, Any>()
+        private val LOCKS = ConcurrentHashMap<String, Any>()
     }
 
     fun install(
@@ -42,7 +42,7 @@ class Ubuntu {
         timeout: Duration
     ) {
         val joinedPackages = packages.joinToString(separator = " ")
-        val lock = LOCKS.computeIfAbsent(ssh) { Object() }
+        val lock = LOCKS.computeIfAbsent(ssh.getHost().ipAddress) { Object() }
         synchronized(lock) {
             ssh.execute("sudo rm -rf /var/lib/apt/lists/*")
             ssh.execute("sudo apt-get update -qq", Duration.ofMinutes(1))
