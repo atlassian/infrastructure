942e0ccbaab164fe2425238758a9566f6f51a7e3
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-09-16 15:47:57 +0200

    JPERF-571: Fix port binding race conditions in PageLoadTimeoutRecoveryTest

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/mock/MockHttpServer.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/mock/MockHttpServer.kt
@@ -11,25 +11,20 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 
-internal class MockHttpServer(private val port: Int) {
+internal class MockHttpServer {
     private val handlers: MutableList<RequestHandler> = mutableListOf()
 
-    internal fun register(handler: RequestHandler): URI {
-        handlers.add(handler)
-        return URI("http://localhost:$port${handler.getContext()}")
-    }
-
-    internal fun start(): AutoCloseable {
+    internal fun start(): CloseableHttpServer {
         val executorService: ExecutorService = Executors.newCachedThreadPool()
         val server = startHttpServer(executorService)
-        return AutoCloseable {
-            executorService.shutdownNow()
-            server.stop(60)
-        }
+        return CloseableHttpServer(
+            httpServer = server,
+            executorService = executorService
+        )
     }
 
     private fun startHttpServer(executor: Executor): HttpServer {
-        val httpServer = retryUntilSuccess(10, TimeUnit.SECONDS) { HttpServer.create(InetSocketAddress(port), 0) }
+        val httpServer = retryUntilSuccess(10, TimeUnit.SECONDS) { HttpServer.create(InetSocketAddress(0), 0) }
         httpServer.executor = executor
 
         handlers.forEach { handler ->
@@ -42,5 +37,25 @@ internal class MockHttpServer(private val port: Int) {
 
     internal interface RequestHandler : HttpHandler {
         fun getContext(): String
+    }
+
+    internal class CloseableHttpServer(
+        private val httpServer: HttpServer,
+        private val executorService: ExecutorService
+    ) : AutoCloseable {
+
+        override fun close() {
+            executorService.shutdownNow()
+            httpServer.stop(60)
+        }
+
+        fun register(handler: RequestHandler): URI {
+            httpServer.createContext(handler.getContext()).handler = handler
+            return URI("http://localhost:${this.getPort()}${handler.getContext()}")
+        }
+
+        fun getPort(): Int {
+            return httpServer.address.port
+        }
     }
 }
