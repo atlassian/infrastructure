957d431eeedc3b5a33eeb78531d0eb6e5e3e15d9
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-10-14 13:35:18 +0200

    JPERF-219: Repro apt-get flakes

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/os/UbuntuIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/os/UbuntuIT.kt
@@ -1,11 +1,15 @@
 package com.atlassian.performance.tools.infrastructure.api.os
 
 import com.atlassian.performance.tools.infrastructure.toSsh
+import com.atlassian.performance.tools.ssh.api.SshConnection
+import com.atlassian.performance.tools.ssh.api.SshHost
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntu
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
+import org.apache.logging.log4j.Level
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.time.Duration
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -25,6 +29,54 @@ class UbuntuIT {
     fun after() {
         sshUbuntu.close()
         executor.shutdownNow()
+    }
+
+    @Test
+    fun shouldRetry() {
+        sshUbuntu.toSsh().newConnection().use { connection ->
+            Ubuntu().install(
+                ColdAptSshConnection(connection),
+                listOf("vim"),
+                Duration.ofSeconds(30)
+            )
+        }
+    }
+
+    private class ColdAptSshConnection(
+        val connection: SshConnection
+    ) : SshConnection by connection {
+
+        private var cold = true
+
+        override fun execute(
+            cmd: String,
+            timeout: Duration,
+            stdout: Level,
+            stderr: Level
+        ): SshConnection.SshResult {
+            val overriddenCommand = if (cmd.contains("apt-get install")) {
+                throttleApt(cmd)
+            } else {
+                cmd
+            }
+            return connection.execute(overriddenCommand, timeout, stdout, stderr)
+        }
+
+        private fun throttleApt(
+            cmd: String
+        ): String = if (cold) {
+            cold = false
+            cmd.replace(
+                oldValue = "apt-get install",
+                newValue = "apt-get -o Acquire::http::Dl-Limit=1 install"
+            )
+        } else {
+            cmd
+        }
+
+        override fun getHost(): SshHost {
+            return connection.getHost()
+        }
     }
 
     @Test
