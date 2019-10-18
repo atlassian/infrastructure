f27e74e3ff9875445373e299f4ec63cb689a8072
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-10-18 13:42:59 +0200

    JPERF-587: Parallelise Chromedriver installation

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/ChromedriverInstaller.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/ChromedriverInstaller.kt
@@ -7,10 +7,17 @@ import java.time.Duration
 
 internal class ChromedriverInstaller(private val uri: URI) {
     internal fun install(ssh: SshConnection) {
-        HttpResource(uri).download(ssh, "chromedriver.zip")
-        Ubuntu().install(ssh, listOf("zip", "libglib2.0-0", "libnss3"), Duration.ofMinutes(2))
-        ssh.execute("unzip -n -q chromedriver.zip")
-        ssh.execute("chmod +x chromedriver")
-        ssh.execute("sudo ln -s `pwd`/chromedriver /usr/bin/chromedriver")
+        ParallelExecutor().execute(
+            {
+                HttpResource(uri).download(ssh, "chromedriver.zip")
+                Ubuntu().install(ssh, listOf("unzip"))
+                ssh.execute("unzip -n -q chromedriver.zip")
+                ssh.execute("chmod +x chromedriver")
+                ssh.execute("sudo ln -s `pwd`/chromedriver /usr/bin/chromedriver")
+            },
+            {
+                Ubuntu().install(ssh, listOf("zip", "libglib2.0-0", "libnss3"), Duration.ofMinutes(2))
+            }
+        )
     }
 }
