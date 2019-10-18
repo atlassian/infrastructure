125912b0fec6399d1dd5468f4102a242fbabd49c
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-10-18 12:04:53 +0200

    JPERF-587: Avoid downloading OracleJdk twice
    We were downloading it twice. Once to test jstat and then to test thread dumps.

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/JstatSupport.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/JstatSupport.kt
@@ -1,9 +1,8 @@
 package com.atlassian.performance.tools.infrastructure.api.jvm
 
-import com.atlassian.performance.tools.infrastructure.toSsh
 import com.atlassian.performance.tools.jvmtasks.api.ExponentialBackoff
 import com.atlassian.performance.tools.jvmtasks.api.IdempotentAction
-import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
+import com.atlassian.performance.tools.ssh.api.SshConnection
 import org.assertj.core.api.Assertions
 import java.io.File
 import java.time.Duration
@@ -29,29 +28,25 @@ class JstatSupport(
     private val jar = "/com/atlassian/performance/tools/infrastructure/api/jvm/$jarName"
     private val timestampLength = "2018-12-17T14:10:44+00:00 ".length
 
-    fun shouldSupportJstat() {
-        SshUbuntuContainer().start().use { ssh ->
-            ssh.toSsh().newConnection().use { connection ->
-                connection.execute("apt-get install curl screen -y -qq", Duration.ofMinutes(2))
-                connection.upload(File(this.javaClass.getResource(jar).toURI()), jarName)
-                jdk.install(connection)
-                connection.startProcess(jdk.command("-classpath $jarName samples.HelloWorld"))
-                val pid = IdempotentAction(
-                    description = "Wait for the Hello, World! process to start.",
-                    action = { connection.execute("cat hello-world.pid").output }
-                ).retry(
-                    maxAttempts = 3,
-                    backoff = ExponentialBackoff(baseBackoff = Duration.ofSeconds(1))
-                )
-                val jstatMonitoring = jdk.jstatMonitoring.start(connection, pid.toInt())
-                waitForJstatToCollectSomeData()
-                jstatMonitoring.stop(connection)
-                val jstatLog = connection.execute("cat ${jstatMonitoring.getResultPath()}").output
-                val jstatHeader = jstatLog.substring(timestampLength, jstatLog.indexOf('\n'))
+    fun shouldSupportJstat(connection: SshConnection) {
+        connection.execute("apt-get install curl screen -y -qq", Duration.ofMinutes(2))
+        connection.upload(File(this.javaClass.getResource(jar).toURI()), jarName)
+        jdk.install(connection)
+        connection.startProcess(jdk.command("-classpath $jarName samples.HelloWorld"))
+        val pid = IdempotentAction(
+            description = "Wait for the Hello, World! process to start.",
+            action = { connection.execute("cat hello-world.pid").output }
+        ).retry(
+            maxAttempts = 3,
+            backoff = ExponentialBackoff(baseBackoff = Duration.ofSeconds(1))
+        )
+        val jstatMonitoring = jdk.jstatMonitoring.start(connection, pid.toInt())
+        waitForJstatToCollectSomeData()
+        jstatMonitoring.stop(connection)
+        val jstatLog = connection.execute("cat ${jstatMonitoring.getResultPath()}").output
+        val jstatHeader = jstatLog.substring(timestampLength, jstatLog.indexOf('\n'))
 
-                Assertions.assertThat(jstatHeader).contains(this.expectedStats)
-            }
-        }
+        Assertions.assertThat(jstatHeader).contains(this.expectedStats)
     }
 
     private fun waitForJstatToCollectSomeData() {
--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/OracleJdkIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/OracleJdkIT.kt
@@ -1,11 +1,19 @@
 package com.atlassian.performance.tools.infrastructure.api.jvm
 
+import com.atlassian.performance.tools.infrastructure.toSsh
+import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
 import org.junit.Test
 
 class OracleJdkIT {
 
     @Test
-    fun shouldSupportJstat() {
-        JstatSupport(OracleJDK()).shouldSupportJstat()
+    fun shouldSupportJstatAndThreadDumps() {
+        SshUbuntuContainer().start().use { ssh ->
+            ssh.toSsh().newConnection().use { connection ->
+                val jdk = OracleJDK()
+                JstatSupport(jdk).shouldSupportJstat(connection)
+                ThreadDumpTest().shouldGatherThreadDump(jdk, connection)
+            }
+        }
     }
 }
