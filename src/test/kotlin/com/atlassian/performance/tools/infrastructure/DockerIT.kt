53c11eacba6f8fa201be2ae17a5fef6c411c1d38
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-08-06 12:13:06 +0200

    JPERF-449: Fixed Docker start via privileged container
    Starting the Docker withing Ubuntu in Docker kept failing.
    Its `/var/log/docker.log` showed the problem. The same could be seed via `sudo dockerd`.
    > failed to start daemon: Error initializing network controller: error obtaining controller instance: failed to create NAT chain DOCKER: iptables failed: iptables -t nat -N DOCKER: iptables v1.6.1: can't initialize iptables table `nat': Permission denied (you must be root)
    Perhaps iptables or your kernel needs to be upgraded.
     (exit status 3)
    
    Now it starts correctly. I hope our CI allows starting privileged containers.

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
@@ -3,18 +3,19 @@ package com.atlassian.performance.tools.infrastructure
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
 import org.junit.Test
 import java.time.Duration
-
+import java.util.function.Consumer
 
 class DockerIT {
     @Test
     fun installWorks() {
-        SshUbuntuContainer().start().use { ssh ->
+        val privilegedContainer = SshUbuntuContainer(Consumer { it.setPrivilegedMode(true) })
+        privilegedContainer.start().use { ssh ->
             ssh.toSsh().newConnection().use { connection ->
                 //workaround for a bug in Docker download site for bionic
                 val packageFile = "containerd.io_1.2.2-3_amd64.deb"
                 connection.execute("curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/$packageFile", Duration.ofMinutes(3))
                 connection.execute("sudo apt install ./$packageFile", Duration.ofMinutes(3))
-                
+
                 Docker().install(connection)
                 DockerImage("hello-world").run(connection)
             }
