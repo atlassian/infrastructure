6cac170921dae63ffec592e070976934fc2c5c34
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2021-02-12 15:35:31 +0100

    JPERF-273: Let Jira installation connect via SSH

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/install/TcpServer.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/install/TcpServer.kt
@@ -1,5 +1,7 @@
 package com.atlassian.performance.tools.infrastructure.api.jira.install
 
+import com.atlassian.performance.tools.ssh.api.Ssh
+
 /**
  * Has open TCP sockets.
  */
@@ -7,16 +9,19 @@ class TcpServer(
     val ip: String,
     val publicPort: Int,
     val privatePort: Int,
-    val name: String
+    val name: String,
+    val ssh: Ssh
 ) {
     constructor(
         ip: String,
         port: Int,
-        name: String
+        name: String,
+        ssh: Ssh
     ) : this(
         ip,
         port,
         port,
-        name
+        name,
+        ssh
     )
 }
