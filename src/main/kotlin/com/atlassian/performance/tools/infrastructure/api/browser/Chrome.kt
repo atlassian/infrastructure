fefac71da48e3d021f6ded027825e1b319a2cbca
Mikolaj Zyromski <mzyromski@atlassian.com>
2021-04-14 15:47:46 +0200

    JPERF-732: Download ChromeDriver version that mathches installed Chrome version
    This is to avoid installing incompatible versions of ChromeDriver and Chrome.

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/Chrome.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/Chrome.kt
@@ -1,11 +1,8 @@
 package com.atlassian.performance.tools.infrastructure.api.browser
 
 import com.atlassian.performance.tools.infrastructure.ChromedriverInstaller
-import com.atlassian.performance.tools.infrastructure.ParallelExecutor
 import com.atlassian.performance.tools.infrastructure.api.os.Ubuntu
 import com.atlassian.performance.tools.ssh.api.SshConnection
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
 import java.net.URI
 import java.time.Duration.ofMinutes
 
@@ -19,19 +16,16 @@ class Chrome : Browser {
     override fun install(ssh: SshConnection) {
         ubuntu.addKey(ssh, "78BD65473CB3BD13")
         ubuntu.addRepository(ssh, "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main", "google-chrome")
-        ParallelExecutor().execute(
-            { Ubuntu().install(ssh, listOf("google-chrome-stable"), ofMinutes(5)) },
-            {
-                val version = getLatestVersion()
-                ChromedriverInstaller(URI("https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip")).install(ssh)
-            }
-        )
+        Ubuntu().install(ssh, listOf("google-chrome-stable"), ofMinutes(5))
+        val installedMinorVersion = getInstalledMinorVersion(ssh)
+
+        val version = ChromedriverInstaller.getLatestVersion(installedMinorVersion)
+        ChromedriverInstaller(URI("https://chromedriver.storage.googleapis.com/$version/chromedriver_linux64.zip")).install(ssh)
+
     }
 
-    private fun getLatestVersion(): String {
-        val httpclient = HttpClients.createDefault()
-        val get = HttpGet("https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
-        val response = httpclient.execute(get)
-        return response.entity.content.bufferedReader().use { it.readLine() }
+    private fun getInstalledMinorVersion(ssh: SshConnection): String? {
+        val versionString = ssh.execute("/usr/bin/google-chrome --version").output
+        return Regex("Google Chrome ([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+").find(versionString)?.groupValues?.getOrNull(1)
     }
 }
