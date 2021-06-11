a7a55f3a3a2c682d358ac2293a2f430161b4488f
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2021-06-11 13:47:04 +0200

    Diagnose `RestUpgrade` failures

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/start/hook/RestUpgrade.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jira/start/hook/RestUpgrade.kt
@@ -1,6 +1,7 @@
 package com.atlassian.performance.tools.infrastructure.api.jira.start.hook
 
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraLaunchTimeouts
+import com.atlassian.performance.tools.infrastructure.api.jira.install.hook.JiraLogs
 import com.atlassian.performance.tools.infrastructure.api.jira.report.FileListing
 import com.atlassian.performance.tools.infrastructure.api.jira.report.Reports
 import com.atlassian.performance.tools.infrastructure.api.jira.start.StartedJira
@@ -18,59 +19,71 @@ class RestUpgrade(
 
     override fun call(ssh: SshConnection, jira: StartedJira, hooks: PostStartHooks, reports: Reports) {
         val threadDump = ThreadDump(jira.pid, jira.installed.jdk)
-        val ip = jira.installed.host.privateIp
-        val port = jira.installed.host.port
-        val upgradesEndpoint = URI("http://$adminUsername:$adminPassword@$ip:$port/rest/api/2/upgrade")
-        reports.add(FileListing("thread-dumps/*"), jira.installed.host)
-        waitForStatusToChange(
-            statusQuo = "000",
-            timeout = timeouts.offlineTimeout,
-            ssh = ssh,
-            uri = upgradesEndpoint,
-            threadDump = threadDump
-        )
-        waitForStatusToChange(
-            statusQuo = "503",
-            timeout = timeouts.initTimeout,
-            ssh = ssh,
-            uri = upgradesEndpoint,
-            threadDump = threadDump
-        )
-        ssh.execute(
-            cmd = "curl --silent --retry 6 -X POST $upgradesEndpoint",
-            timeout = Duration.ofSeconds(15)
-        )
-        waitForStatusToChange(
-            statusQuo = "303",
-            timeout = timeouts.upgradeTimeout,
-            ssh = ssh,
-            uri = upgradesEndpoint,
-            threadDump = threadDump
-        )
+        val polling = Upgrades(ssh, jira, adminUsername, adminPassword, timeouts, threadDump, reports)
+        polling.waitUntilOnline()
+        polling.waitUntilHealthy()
+        polling.triggerUpgrades()
+        polling.waitUntilUpgraded()
     }
 
-    private fun waitForStatusToChange(
-        statusQuo: String,
-        uri: URI,
-        timeout: Duration,
-        ssh: SshConnection,
-        threadDump: ThreadDump
+    private class Upgrades(
+        private val ssh: SshConnection,
+        private val jira: StartedJira,
+        adminUsername: String,
+        adminPassword: String,
+        private val timeouts: JiraLaunchTimeouts,
+        private val threadDump: ThreadDump,
+        private val reports: Reports
     ) {
-        val backoff = Duration.ofSeconds(10)
-        val deadline = Instant.now() + timeout
-        while (true) {
-            val currentStatus = ssh.safeExecute(
-                cmd = "curl --silent --write-out '%{http_code}' --output /dev/null -X GET $uri",
-                timeout = timeouts.unresponsivenessTimeout
-            ).output
-            if (currentStatus != statusQuo) {
-                break
+        private val upgradesEndpoint: URI
+
+        init {
+            val ip = jira.installed.host.privateIp
+            val port = jira.installed.host.port
+            upgradesEndpoint = URI("http://$adminUsername:$adminPassword@$ip:$port/rest/api/2/upgrade")
+        }
+
+        fun waitUntilOnline() {
+            waitForStatusToChange("000", timeouts.offlineTimeout)
+        }
+
+        fun waitUntilHealthy() {
+            waitForStatusToChange("503", timeouts.initTimeout)
+        }
+
+        fun waitUntilUpgraded() {
+            waitForStatusToChange("303", timeouts.upgradeTimeout)
+        }
+
+        private fun waitForStatusToChange(
+            statusQuo: String,
+            timeout: Duration
+        ) {
+            val backoff = Duration.ofSeconds(10)
+            val deadline = Instant.now() + timeout
+            while (true) {
+                val currentStatus = ssh.safeExecute(
+                    cmd = "curl --silent --write-out '%{http_code}' --output /dev/null -X GET $upgradesEndpoint",
+                    timeout = timeouts.unresponsivenessTimeout
+                ).output
+                if (currentStatus != statusQuo) {
+                    break
+                }
+                if (deadline < Instant.now()) {
+                    reports.add(JiraLogs().report(jira.installed), jira.installed.host)
+                    reports.add(FileListing("thread-dumps/*"), jira.installed.host)
+                    throw Exception("$upgradesEndpoint failed to get out of $statusQuo status within $timeout")
+                }
+                threadDump.gather(ssh, "thread-dumps")
+                Thread.sleep(backoff.toMillis())
             }
-            if (deadline < Instant.now()) {
-                throw Exception("$uri failed to get out of $statusQuo status within $timeout")
-            }
-            threadDump.gather(ssh, "thread-dumps")
-            Thread.sleep(backoff.toMillis())
+        }
+
+        fun triggerUpgrades() {
+            ssh.execute(
+                cmd = "curl --silent --retry 6 -X POST $upgradesEndpoint",
+                timeout = Duration.ofSeconds(15)
+            )
         }
     }
 }
