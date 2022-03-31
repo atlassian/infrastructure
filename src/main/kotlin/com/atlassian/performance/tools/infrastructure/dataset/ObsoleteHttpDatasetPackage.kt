618a6cfffc259be4d3bdc644665573f4636705d6
Greg Rowinski <growinski@atlassian.com>
2022-03-31 17:24:15 +0200

    PERF-750: remove reference to jira-soke-tests-eu-s3 bucket

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/dataset/ObsoleteHttpDatasetPackage.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/dataset/ObsoleteHttpDatasetPackage.kt
@@ -1,12 +1,7 @@
 package com.atlassian.performance.tools.infrastructure.dataset
 
 import com.atlassian.performance.tools.infrastructure.api.dataset.DatasetPackage
-import com.atlassian.performance.tools.infrastructure.api.dataset.FileArchiver
-import com.atlassian.performance.tools.jvmtasks.api.ExponentialBackoff
-import com.atlassian.performance.tools.jvmtasks.api.IdempotentAction
-import com.atlassian.performance.tools.jvmtasks.api.TaskTimer
 import com.atlassian.performance.tools.ssh.api.SshConnection
-import java.net.URI
 import java.time.Duration
 
 @Deprecated("The adapter can be removed with a major release.")
@@ -16,21 +11,13 @@ internal class ObsoleteHttpDatasetPackage(
     private val downloadTimeout: Duration
 ) : DatasetPackage {
 
-    private val datasetBucket = URI("https://s3.eu-central-1.amazonaws.com/jira-soke-tests-eu/")
-
     override fun download(
         ssh: SshConnection
     ): String {
-        val unzipCommand = FileArchiver().pipeUnzip(ssh)
-        TaskTimer.time("download") {
-            IdempotentAction("download dataset") {
-                ssh.execute("wget -qO - ${datasetBucket.resolve(downloadPath)} | $unzipCommand", downloadTimeout)
-            }.retry(5, ExponentialBackoff(Duration.ofSeconds(2)))
-        }
-        return unpackedPath!!
+        throw UnsupportedOperationException("Download of this HttpDatasetPackage (downloadPath='$downloadPath') could not be executed. Create HttpDatasetPackage instance using the two-parameters constructor rather than the deprecated three-parameters one.")
     }
 
     override fun toString(): String {
-        return "HttpDatasetPackage(downloadPath='$downloadPath', unpackedPath=$unpackedPath, downloadTimeout=$downloadTimeout, datasetBucket=$datasetBucket)"
+        return "HttpDatasetPackage(downloadPath='$downloadPath', unpackedPath=$unpackedPath, downloadTimeout=$downloadTimeout, datasetBucket=UNSUPPORTED)"
     }
 }
