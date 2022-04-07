551332544ff8044f1b541e592a61e1c643d5bb0c
Greg Rowinski <growinski@atlassian.com>
2022-04-07 16:23:59 +0200

    JPERF-774: bump log4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 val kotlinVersion = "1.2.70"
+val log4jVersion = "2.17.2"
 
 plugins {
     kotlin("jvm").version("1.2.70")
@@ -35,6 +36,7 @@ configurations.all {
             }
             when (requested.group) {
                 "org.jetbrains.kotlin" -> useVersion(kotlinVersion)
+                "org.apache.logging.log4j" -> useVersion(log4jVersion)
             }
 
         }
@@ -79,7 +81,7 @@ fun webdriver(): List<String> = listOf(
 fun log4j(
     vararg modules: String
 ): List<String> = modules.map { module ->
-    "org.apache.logging.log4j:log4j-$module:2.10.0"
+    "org.apache.logging.log4j:log4j-$module:$log4jVersion"
 }
 
 tasks.compileTestKotlin {
