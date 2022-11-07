7ddaa973f604d4df3341b6088445a2094473e974
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2022-11-07 16:23:01 +0100

    Remove test-only workaround
    The test has the same results anyway: all LTS green except Jammy.

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
@@ -2,7 +2,6 @@ package com.atlassian.performance.tools.infrastructure
 
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
 import org.junit.Test
-import java.time.Duration
 
 class DockerIT {
     @Test
@@ -33,15 +32,6 @@ class DockerIT {
             .start()
             .use { ubuntu ->
                 ubuntu.toSsh().newConnection().use { connection ->
-                    //workaround for a bug in Docker download site for bionic
-                    val packageFile = "containerd.io_1.2.2-3_amd64.deb"
-                    connection.execute(
-                        "curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/$packageFile",
-                        Duration.ofMinutes(3)
-                    )
-                    connection.execute("sudo apt install ./$packageFile", Duration.ofMinutes(3))
-
-                    Docker().install(connection)
                     DockerImage("hello-world").run(connection)
                 }
             }
