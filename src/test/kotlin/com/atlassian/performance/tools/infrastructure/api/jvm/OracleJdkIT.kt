082aad61f5a70ae8db38b3963bf70adb043e2f70
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2022-11-08 14:22:55 +0100

    Replace deprecated call in `ThreadDumpTest`

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/OracleJdkIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/OracleJdkIT.kt
@@ -1,7 +1,5 @@
 package com.atlassian.performance.tools.infrastructure.api.jvm
 
-import com.atlassian.performance.tools.infrastructure.toSsh
-import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
 import org.junit.Test
 
 class OracleJdkIT {
@@ -10,11 +8,6 @@ class OracleJdkIT {
     fun shouldSupportJstatAndThreadDumps() {
         val jdk = OracleJDK()
         JstatSupport(jdk).shouldSupportJstat()
-        SshUbuntuContainer.Builder().build().start().use { ubuntu ->
-            val ssh = ubuntu.toSsh()
-            ssh.newConnection().use { connection ->
-                ThreadDumpTest().shouldGatherThreadDump(jdk, connection)
-            }
-        }
+        ThreadDumpTest(jdk).shouldGatherThreadDump()
     }
 }
--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/ThreadDumpIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/ThreadDumpIT.kt
@@ -1,19 +1,32 @@
 package com.atlassian.performance.tools.infrastructure.api.jvm
 
+import com.atlassian.performance.tools.infrastructure.toSsh
 import com.atlassian.performance.tools.jvmtasks.api.IdempotentAction
 import com.atlassian.performance.tools.jvmtasks.api.StaticBackoff
+import com.atlassian.performance.tools.ssh.api.Ssh
 import com.atlassian.performance.tools.ssh.api.SshConnection
-import org.assertj.core.api.Assertions
+import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
+import org.assertj.core.api.Assertions.assertThat
 import java.time.Duration
 
-class ThreadDumpTest {
-    fun shouldGatherThreadDump(jdk: JavaDevelopmentKit, connection: SshConnection) {
+class ThreadDumpTest(
+    private val jdk: JavaDevelopmentKit
+) {
+    fun shouldGatherThreadDump() {
+        SshUbuntuContainer.Builder().build().start().use { ubuntu ->
+            val ssh = ubuntu.toSsh()
+            ssh.newConnection().use { connection ->
+                shouldGatherThreadDump(ssh, connection)
+            }
+        }
+    }
+
+    private fun shouldGatherThreadDump(ssh: Ssh, connection: SshConnection) {
         jdk.install(connection)
         val destination = "thread-dumps"
         connection.execute("""echo "public class Test { public static void main(String[] args) { try { Thread.sleep(java.time.Duration.ofMinutes(1).toMillis()); } catch (InterruptedException e) { throw new RuntimeException(e); } }}" > Test.java """.trimIndent())
         connection.execute("${jdk.use()}; javac Test.java")
-        val process = connection.startProcess("${jdk.use()}; java Test")
-        try {
+        ssh.runInBackground("${jdk.use()}; java Test").use {
             val pid = IdempotentAction("Get PID") {
                 getPid(connection, jdk)
             }.retry(maxAttempts = 2, backoff = StaticBackoff(Duration.ofSeconds(1)))
@@ -22,11 +35,7 @@ class ThreadDumpTest {
 
             val threadDumpFile = connection.execute("ls $destination").output
             val threadDump = connection.execute("cat $destination/$threadDumpFile").output
-            Assertions.assertThat(threadDump).contains("Full thread dump Java HotSpot")
-        } catch (e: Exception) {
-            throw Exception(e)
-        } finally {
-            connection.stopProcess(process)
+            assertThat(threadDump).contains("Full thread dump Java HotSpot")
         }
     }
 
