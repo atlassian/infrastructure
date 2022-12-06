b0f147c2d86410097e3b48a76cffecc062de6f91
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2022-12-06 12:55:13 +0100

    JPERF-896: Remove magic from shell commands

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/PublicAtlassianProduct.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/PublicAtlassianProduct.kt
@@ -17,17 +17,20 @@ internal class PublicAtlassianProduct(
 
     private fun download(sshConnection: SshConnection) {
         val jiraArchiveUri = URI("https://product-downloads.atlassian.com/software/jira/downloads/$archiveName")
-        sshConnection.execute("mkdir -p $destination")
+        sshConnection.execute("mkdir --parents $destination")
         HttpResource(jiraArchiveUri).download(sshConnection, "$destination/$archiveName", Duration.ofMinutes(6))
     }
 
     private fun unpack(sshConnection: SshConnection) {
-        sshConnection.execute("tar -xzf $destination/$archiveName --directory $destination", Duration.ofMinutes(1))
+        sshConnection.execute(
+            "tar --extract --gzip --file $destination/$archiveName --directory $destination",
+            timeout = Duration.ofMinutes(1)
+        )
     }
 
     private fun discoverDirectoryName(sshConnection: SshConnection): String = sshConnection
         .execute(
-            "tar -tf $destination/$archiveName --directory $destination | head -n 1",
+            "tar --list --file $destination/$archiveName | head --lines=1",
             timeout = Duration.ofMinutes(1)
         )
         .output
