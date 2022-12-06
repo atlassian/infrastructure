0baaa54118f5824d31782c970239c1b84876744e
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2022-12-06 15:25:23 +0100

    JPERF-896: Extract `TarGzDistribution`
    Facilitate other sources of tar.gz distros.

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/distribution/PublicJiraServiceDeskDistribution.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/distribution/PublicJiraServiceDeskDistribution.kt
@@ -1,17 +1,11 @@
 package com.atlassian.performance.tools.infrastructure.api.distribution
 
 import com.atlassian.performance.tools.infrastructure.PublicAtlassianProduct
-import com.atlassian.performance.tools.ssh.api.SshConnection
 
 /**
  * Downloads Jira Service Desk installers from the official Jira downloads site.
  * @since 4.8.0
  */
-class PublicJiraServiceDeskDistribution(private val version: String) : ProductDistribution {
-    override fun install(ssh: SshConnection, destination: String): String {
-        return PublicAtlassianProduct(
-            archiveName = "atlassian-servicedesk-$version.tar.gz",
-            destination = destination
-        ).install(ssh)
-    }
-}
+class PublicJiraServiceDeskDistribution(
+    version: String
+) : ProductDistribution by PublicAtlassianProduct("atlassian-servicedesk-$version.tar.gz")
---
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/distribution/TarGzDistribution.kt
@@ -0,0 +1,28 @@
+package com.atlassian.performance.tools.infrastructure.api.distribution
+
+import com.atlassian.performance.tools.ssh.api.SshConnection
+import java.time.Duration
+
+/**
+ * @param tarGzPath remote path to a tar.gz distribution
+ */
+class TarGzDistribution(
+    private val tarGzPath: String
+) : ProductDistribution {
+
+    override fun install(ssh: SshConnection, destination: String): String {
+        ssh.execute(
+            "tar --extract --gzip --file $tarGzPath --directory $destination",
+            timeout = Duration.ofMinutes(1)
+        )
+        val unpackedDirectory = ssh
+            .execute(
+                "tar --list --file $tarGzPath | head --lines=1",
+                timeout = Duration.ofMinutes(1)
+            )
+            .output
+            .split("/")
+            .first()
+        return "$destination/$unpackedDirectory"
+    }
+}
