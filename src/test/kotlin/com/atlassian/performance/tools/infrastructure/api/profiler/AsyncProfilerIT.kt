4cc1a977405025d9fd00876b6a5fe5145207e6f6
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-02-10 18:06:27 +0100

    JPERF-969: Test `AsyncProfiler`
    I thought that it would flake on focal, but be green on xenial.
    But repeated tests show that both flake:
    ```
    java.lang.Exception: Error while executing ./async-profiler/profiler.sh -b 20000000 start 1014. Exit status code SshResult(exitStatus=1, output=, errorOutput=Could not start attach mechanism: No such file or directory
    )
    	at com.atlassian.performance.tools.ssh.SshjConnection.execute(SshjConnection.kt:40)
    	at com.atlassian.performance.tools.ssh.api.SshConnection.execute(SshConnection.kt:27)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfiler.start(AsyncProfiler.kt:22)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT$testOn$1.invoke(AsyncProfilerIT.kt:40)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT$testOn$1.invoke(AsyncProfilerIT.kt:18)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.testOnInstalledJira(AsyncProfilerIT.kt:74)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.testOn(AsyncProfilerIT.kt:34)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.shouldWorkOnXenial(AsyncProfilerIT.kt:22)
    ```
    
    ```
    java.lang.Exception: Error while executing ./async-profiler/profiler.sh -b 20000000 start 1023. Exit status code SshResult(exitStatus=1, output=, errorOutput=Could not start attach mechanism: No such file or directory
    )
    	at com.atlassian.performance.tools.ssh.SshjConnection.execute(SshjConnection.kt:40)
    	at com.atlassian.performance.tools.ssh.api.SshConnection.execute(SshConnection.kt:27)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfiler.start(AsyncProfiler.kt:22)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT$testOn$1.invoke(AsyncProfilerIT.kt:40)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT$testOn$1.invoke(AsyncProfilerIT.kt:18)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.testOnInstalledJira(AsyncProfilerIT.kt:74)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.testOn(AsyncProfilerIT.kt:34)
    	at com.atlassian.performance.tools.infrastructure.api.profiler.AsyncProfilerIT.shouldWorkOnFocal(AsyncProfilerIT.kt:27)
    ```

---
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/profiler/AsyncProfilerIT.kt
@@ -0,0 +1,77 @@
+package com.atlassian.performance.tools.infrastructure.api.profiler
+
+import com.atlassian.performance.tools.infrastructure.api.distribution.PublicJiraSoftwareDistribution
+import com.atlassian.performance.tools.infrastructure.api.jira.EmptyJiraHome
+import com.atlassian.performance.tools.infrastructure.api.jvm.AdoptOpenJDK
+import com.atlassian.performance.tools.infrastructure.api.os.RemotePath
+import com.atlassian.performance.tools.infrastructure.jira.install.InstalledJira
+import com.atlassian.performance.tools.infrastructure.jira.install.ParallelInstallation
+import com.atlassian.performance.tools.infrastructure.jira.install.TcpServer
+import com.atlassian.performance.tools.infrastructure.jira.start.JiraLaunchScript
+import com.atlassian.performance.tools.infrastructure.toSsh
+import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.nio.file.Files.createTempFile
+import java.util.function.Consumer
+
+class AsyncProfilerIT {
+
+    @Test
+    fun shouldWorkOnXenial() {
+        testOn("16.04")
+    }
+
+    @Test
+    fun shouldWorkOnFocal() {
+        testOn("20.04")
+    }
+
+    private fun testOn(ubuntuVersion: String) {
+        // given
+        val profiler = AsyncProfiler()
+
+        testOnInstalledJira(ubuntuVersion) { installedJira ->
+            val sshClient = installedJira.server.ssh
+            sshClient.newConnection().use { ssh ->
+                // when
+                profiler.install(ssh)
+                val startedJira = JiraLaunchScript().start(installedJira)
+                val process = profiler.start(ssh, startedJira.pid)
+                Thread.sleep(1000)
+                process.stop(ssh)
+
+                // then
+                val profilerResult = RemotePath(sshClient.host, process.getResultPath())
+                    .download(createTempFile("profiler-result", ".svg"))
+                assertThat(profilerResult.readLines()).isNotEmpty
+            }
+        }
+    }
+
+    private fun <T> testOnInstalledJira(ubuntuVersion: String, test: (InstalledJira) -> T) {
+        val privatePort = 8080
+        val container = SshUbuntuContainer.Builder()
+            .version(ubuntuVersion)
+            .customization(Consumer {
+                it.addExposedPort(privatePort)
+                it.setPrivilegedMode(true)
+            })
+            .build()
+        container.start().use { sshUbuntu ->
+            val server = TcpServer(
+                "localhost",
+                sshUbuntu.container.getMappedPort(privatePort),
+                privatePort,
+                "my-jira",
+                sshUbuntu.toSsh()
+            )
+            val installed = ParallelInstallation(
+                jiraHomeSource = EmptyJiraHome(),
+                productDistribution = PublicJiraSoftwareDistribution("7.13.0"),
+                jdk = AdoptOpenJDK()
+            ).install(server)
+            test(installed)
+        }
+    }
+}
