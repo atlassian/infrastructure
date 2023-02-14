147536b2bb2896e877fdaef5b62ce8f98acea84f
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2023-02-14 16:03:14 +0100

    Replace lftp with curl

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/HttpResource.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/HttpResource.kt
@@ -27,7 +27,7 @@ class HttpResource(
     ) {
         Ubuntu().install(ssh, listOf("lftp"), Duration.ofMinutes(2))
         ssh.execute(
-            """lftp -c 'set net:timeout 15; set net:max-retries 50; pget -n 32 -c "$uri" -o $destination'""",
+            """curl -s "$uri" -o $destination""",
             timeout
         )
     }
