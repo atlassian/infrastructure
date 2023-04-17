7bd7150057561c3fe23f600a6531f0ddf3435b0b
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-04-17 16:50:09 +0200

    JPERF-838: Respect the `splunkForwarder` delegate

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/splunk/Log4j2SplunkForwarder.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/splunk/Log4j2SplunkForwarder.kt
@@ -1,16 +1,34 @@
 package com.atlassian.performance.tools.infrastructure.api.splunk
 
-import com.atlassian.performance.tools.infrastructure.splunk.Log4jJsonifier
 import com.atlassian.performance.tools.ssh.api.SshConnection
 import java.nio.file.Paths
 
-class Log4j2SplunkForwarder(log4j2ConfigFileName: String, splunkForwarder: SplunkForwarder) :
-    SplunkForwarder by splunkForwarder {
-
-    private val log4j2ConfigFileName = log4j2ConfigFileName
+/**
+ * Tries to JSONify both log4j v1 and log4j v2. Fails only when both attempts fail.
+ */
+class Log4j2SplunkForwarder(
+    private val log4j2ConfigFileName: String,
+    private val splunkForwarder: SplunkForwarder
+) : SplunkForwarder by splunkForwarder {
 
     override fun jsonifyLog4j(sshConnection: SshConnection, log4jPropertiesPath: String) {
-        val log4j2ConfigPath = Paths.get(log4jPropertiesPath).resolveSibling(log4j2ConfigFileName).toString()
-        Log4jJsonifier().jsonifyLog4j1AndLog4j2(sshConnection, log4jPropertiesPath, log4j2ConfigPath)
+        val log4j2PropertiesPath = Paths.get(log4jPropertiesPath)
+            .resolveSibling(log4j2ConfigFileName)
+            .toString()
+        val log4j1Fail = tryToJsonify(sshConnection, log4jPropertiesPath)
+        val log4j2Fail = tryToJsonify(sshConnection, log4j2PropertiesPath)
+        if (log4j1Fail != null && log4j2Fail != null) {
+            log4j1Fail.addSuppressed(log4j2Fail)
+            throw log4j1Fail
+        }
+    }
+
+    private fun tryToJsonify(ssh: SshConnection, log4jPath: String): Throwable? {
+        return try {
+            splunkForwarder.jsonifyLog4j(ssh, log4jPath)
+            null
+        } catch (e: Exception) {
+            e
+        }
     }
 }
