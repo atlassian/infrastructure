0291ca9c18ec3e127ef78bb17a5c574d69988004
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-09-07 10:55:18 +0200

    JPERF-273 Install only required packages to make jira captcha work

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/JdkFonts.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/jvm/JdkFonts.kt
@@ -7,7 +7,6 @@ class JdkFonts {
 
     fun install(ssh: SshConnection) {
         val packages = listOf(
-            "fonts-dejavu-core", // https://confluence.atlassian.com/bitbucketserverkb/captcha-image-doesn-t-render-779171210.html
             "fontconfig" // https://jira.atlassian.com/browse/CONFSRVDEV-8954
         )
         Ubuntu().install(ssh, packages)
