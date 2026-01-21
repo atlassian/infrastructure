dd12577ac5521ad03069a3888d2b5464d5cbac16
Michał Błajet <mblajet@atlassian.com>
2026-01-21 14:44:23 +0100

    JSEC-7320: remove obsolete s3 bucket addresses and mark version-specific chromium implementations as deprecated

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium69IT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium69IT.kt
@@ -1,43 +1,30 @@
 package com.atlassian.performance.tools.infrastructure.api.browser.chromium
 
 import com.atlassian.performance.tools.infrastructure.toSsh
-import com.atlassian.performance.tools.jvmtasks.api.IdempotentAction
-import com.atlassian.performance.tools.jvmtasks.api.StaticBackoff
-import com.atlassian.performance.tools.ssh.api.SshConnection
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
 import org.hamcrest.Matchers
 import org.junit.Assert
 import org.junit.Test
-import java.time.Duration
 
 class Chromium69IT {
 
     @Test
-    fun shouldInstallBrowser() {
+    fun shouldThrowUnsupportedError() {
         SshUbuntuContainer.Builder().build().start().use { sshUbuntu ->
             sshUbuntu.toSsh().newConnection().use { connection ->
-                val installedBefore = isChromiumInstalled(connection)
+                val exception = Assert.assertThrows(IllegalStateException::class.java) {
+                    Chromium69().install(connection)
+                }
 
-                Chromium69().install(connection)
-
-                val installedAfter = IdempotentAction("Find Chromium") {
-                    isChromiumInstalled(connection)
-                }.retry(maxAttempts = 2, backoff = StaticBackoff(Duration.ofSeconds(1)))
-
-                Assert.assertThat(installedBefore, Matchers.`is`(false))
-                Assert.assertThat(installedAfter, Matchers.`is`(true))
+                Assert.assertThat(
+                    exception.message,
+                    Matchers.containsString("is no longer supported")
+                )
+                Assert.assertThat(
+                    exception.message,
+                    Matchers.containsString("CustomChromium")
+                )
             }
         }
-    }
-
-    private fun isChromiumInstalled(ssh: SshConnection): Boolean {
-        val result = ssh
-            .safeExecute("ls -lh /usr/bin/chrome")
-        return result.isSuccessful()
-            .and(
-                result
-                    .output
-                    .contains("/root/chrome-linux/chrome")
-            )
     }
 }
--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium70IT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium70IT.kt
@@ -1,11 +1,30 @@
 package com.atlassian.performance.tools.infrastructure.api.browser.chromium
 
+import com.atlassian.performance.tools.infrastructure.toSsh
+import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
+import org.hamcrest.Matchers
+import org.junit.Assert
 import org.junit.Test
 
 class Chromium70IT {
 
     @Test
-    fun shouldRecoverFromPageLoadTimeout() {
-        PageLoadTimeoutRecoveryTest().run(Chromium70())
+    fun shouldThrowUnsupportedError() {
+        SshUbuntuContainer.Builder().build().start().use { sshUbuntu ->
+            sshUbuntu.toSsh().newConnection().use { connection ->
+                val exception = Assert.assertThrows(IllegalStateException::class.java) {
+                    Chromium70().install(connection)
+                }
+
+                Assert.assertThat(
+                    exception.message,
+                    Matchers.containsString("is no longer supported")
+                )
+                Assert.assertThat(
+                    exception.message,
+                    Matchers.containsString("CustomChromium")
+                )
+            }
+        }
     }
 }
