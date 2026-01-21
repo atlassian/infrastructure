dda434c73da1d198b281226eaf5b8065b44a8e3e
Michał Błajet <mblajet@atlassian.com>
2026-01-21 14:44:23 +0100

    JSEC-7320: remove obsolete s3 bucket addresses and mark version-specific chromium implementations as deprecated

--- src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium69.kt
+++ src/main/kotlin/com/atlassian/performance/tools/infrastructure/api/browser/chromium/Chromium69.kt
@@ -1,23 +1,16 @@
 package com.atlassian.performance.tools.infrastructure.api.browser.chromium
 
-import com.atlassian.performance.tools.infrastructure.ChromedriverInstaller
-import com.atlassian.performance.tools.infrastructure.ChromiumInstaller
 import com.atlassian.performance.tools.infrastructure.api.browser.Browser
-import com.atlassian.performance.tools.infrastructure.ParallelExecutor
 import com.atlassian.performance.tools.ssh.api.SshConnection
-import java.net.URI
 
+@Deprecated("Use CustomChromium")
 class Chromium69 : Browser {
-    private val chromiumUri = URI("https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F576753%2Fchrome-linux.zip?generation=1532051976706023&alt=media")
-    private val chromedriverUri = URI("https://s3.eu-central-1.amazonaws.com/jpt-chromedriver/2.43/chromedriver.zip")
-
     /**
      * Installs chromium 69 with a compatible chromedriver.
      */
     override fun install(ssh: SshConnection) {
-        ParallelExecutor().execute(
-            { ChromiumInstaller(chromiumUri).install(ssh) },
-            { ChromedriverInstaller(chromedriverUri).install(ssh) }
-        )
+        throw IllegalStateException("${this.javaClass} is no longer supported. " +
+            "Please use com/atlassian/performance/tools/infrastructure/api/browser/chromium/CustomChromium.kt instead " +
+            "with explicitly provided Chromium and Chrome Driver URIs.")
     }
 }
